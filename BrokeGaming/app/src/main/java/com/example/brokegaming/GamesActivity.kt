1df335a44e4e5038b11a53a476c939d4b85f172a
jsan712 <jsan712@gmail.com>
2021-11-19 02:09:38 -0500

    Workign on Games screen

--- BrokeGaming/app/src/main/java/com/example/brokegaming/GamesActivity.kt
+++ BrokeGaming/app/src/main/java/com/example/brokegaming/GamesActivity.kt
@@ -1,4 +1,14 @@
 package com.example.brokegaming
 
-class GamesActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class GamesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_games)
+
+        Log.d("MainActivity", "onCreate called!")
+    }
 }
